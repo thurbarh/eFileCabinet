3f642237bbeaea501792a5568ddef682bc99469a
PrazeStack <adegsfemzy@gmail.com>
2021-05-20 09:55:57 +0100

    Encryption Added

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -47,11 +47,17 @@ namespace DocumentServer.Controllers
                 {
                     using (var stream = new FileStream(filePath, FileMode.Create))
                     {
-                        await file.CopyToAsync(stream);
+                       await  file.CopyToAsync(stream);
+                    }
+                    string outputFile = Path.Combine(basePath, Guid.NewGuid().ToString());
+                 
+                    EncryptFile.Encrypt(filePath, outputFile);
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        System.IO.File.Delete(filePath);
                     }
                     var fileModel = new FileModel
                     {
-
                         Name = fileName,
                         DateAdded = DateTime.Now,
                         FilePath = filePath,
@@ -61,12 +67,13 @@ namespace DocumentServer.Controllers
                         User_File_GroupId= 1,
                         Size_in_Bytes = int.Parse(file.Length.ToString()),
                         Description = description,
-
-                        FileName = fileName,
+                        FileName = outputFile,
                     };
                     db.File.Add(fileModel);
                     db.SaveChanges();
+                   
                 }
+                 
             }
             TempData["Message"] = "File successfully uploaded to File System.";
             return RedirectToAction("Index");
@@ -75,6 +82,7 @@ namespace DocumentServer.Controllers
         {
 
             var file = await db.File.Where(x => x.Id == id).FirstOrDefaultAsync();
+            EncryptFile.Decrypt(file.FileName, file.FilePath);
             var extension = Path.GetExtension(file.FilePath);
             if (file == null) return null;
             var memory = new MemoryStream();
@@ -83,22 +91,32 @@ namespace DocumentServer.Controllers
                 await stream.CopyToAsync(memory);
             }
             memory.Position = 0;
+
+            EncryptFile.Encrypt(file.FileName, file.FilePath);
+            if (System.IO.File.Exists(file.FilePath))
+            {
+                System.IO.File.Delete(file.FilePath);
+            }
             return File(memory, file.ContentType, file.Name + extension);
+          
 
         }
         public async Task<IActionResult> DeleteFile(int id)
         {
 
             var file = await db.File.Where(x => x.Id == id).FirstOrDefaultAsync();
-            if (file == null) return null;
-            if (System.IO.File.Exists(file.FilePath))
+            if (file != null)
             {
-                System.IO.File.Delete(file.FilePath);
+
+                if (System.IO.File.Exists(file.FileName))
+                {
+                    System.IO.File.Delete(file.FileName);
+                }
+                var extension = Path.GetExtension(file.FilePath);
+                db.File.Remove(file);
+                db.SaveChanges();
+                TempData["Message"] = $"Removed {file.Name + extension} successfully from File System.";
             }
-            var extension = Path.GetExtension(file.FilePath);
-            db.File.Remove(file);
-            db.SaveChanges();
-            TempData["Message"] = $"Removed {file.Name + extension} successfully from File System.";
             return RedirectToAction("Index");
         }
 
---
+++ Models/EncryptFile.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
+
+namespace DocumentServer.Models
+{
+    public static class EncryptFile
+    {
+        ///<summary>
+        /// Steve Lydford - 12/05/2008.
+        ///
+        /// Encrypts a file using Rijndael algorithm.
+        ///</summary>
+        ///<param name="inputFile"></param>
+        ///<param name="outputFile"></param>
+        public static void Encrypt(string inputFile, string outputFile)
+        {
+
+                string password = @"myKey123"; // Your Key Here
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                string cryptFile = outputFile;
+                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateEncryptor(key, key),
+                    CryptoStreamMode.Write);
+
+                FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                    cs.WriteByte((byte)data);
+
+
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();
+            
+        
+           
+        }
+        ///<summary>
+        /// Steve Lydford - 12/05/2008.
+        ///
+        /// Decrypts a file using Rijndael algorithm.
+        ///</summary>
+        ///<param name="inputFile"></param>
+        ///<param name="outputFile"></param>
+        public static void Decrypt(string inputFile, string outputFile)
+        {
+
+            {
+                string password = @"myKey123"; // Your Key Here
+
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateDecryptor(key, key),
+                    CryptoStreamMode.Read);
+
+                FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                    fsOut.WriteByte((byte)data);
+
+                fsOut.Close();
+                cs.Close();
+                fsCrypt.Close();
+
+            }
+        }
+    }
+}
